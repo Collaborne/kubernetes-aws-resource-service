adb1bb38439185f22e4b1eba44718cfcc175fa97
Andreas Kohn <andreas.kohn@gmail.com>
2017-06-02 17:31:32 +0200

    Report new builds to the collaborne "deployer"

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -20,4 +20,12 @@ for v in $*; do
 		echo "Cannot push image ${DOCKER_NAME}:$v, aborting"
 		exit 1
 	fi
+
+	_safeDockerName=`echo ${DOCKER_NAME} | sed 's,/,%2f,g'`
+	_branches=`curl -sfX PUT -H "authorization: Bearer ${DEPLOYER_TOKEN}" https://deployer.internal.collaborne.com/api/repositories/${_safeDockerName}/images/$v`
+	if [ $? -ne 0 ]; then
+		echo "Cannot register image ${DOCKER_NAME}:$v with the deployer, image will not be automatically deployed"
+	else
+		echo "Triggered build for branches: `echo ${_branches} | jq -r '.branches[]' | xargs echo`"
+	fi
 done
