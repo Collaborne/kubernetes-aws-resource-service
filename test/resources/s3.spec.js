2385f32ed745ad8df3ad962a240d987f44a875d8
Andreas Kohn <andreas.kohn@gmail.com>
2019-06-26 17:19:40 +0200

    Add a test case for checking capitalization of the 'AWS' principal

--- test/resources/s3.spec.js
+++ test/resources/s3.spec.js
@@ -306,6 +306,33 @@ describe('s3', function utilsTest() {
 			});
 			expect(policy).to.be.deep.equals(expectedPolicy);
 		});
+		it('translates "AWS" principals', () => {
+			const s3 = new S3Bucket();
+			const expectedPolicy = {
+				Bucket: 'TestBucket',
+				ConfirmRemoveSelfBucketAccess: false,
+				Policy: '{"Statement":[{"Resource":"arn:aws:s3:::TestBucket","Action":"*","Effect":"Allow","Principal":{"AWS":"principal"}}]}',
+			};
+			const {policy} = s3._translateSpec({
+				metadata: {
+					name: 'TestBucket',
+				},
+				spec: {
+					policy: {
+						statement: [
+							{
+								action: '*',
+								effect: 'Allow',
+								principal: {
+									AWS: 'principal',
+								}
+							},
+						],
+					},
+				},
+			});
+			expect(policy).to.be.deep.equals(expectedPolicy);
+		});
 	});
 	describe('create behavior', () => {
 		it('invokes update when the bucket exists for this account', async() => {
